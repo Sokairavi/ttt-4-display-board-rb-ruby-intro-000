d914bd414d5d00b59afd2d6ee834b4005c45b9a4
Soma Majumder <soma.creativeonline@gmail.com>
2020-04-08 08:14:07 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -11,3 +11,30 @@ display_board(board_1)
 
 board_2 = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
 display_board(board_2)
+
+board_3 = ["O", " ", " ", " ", " ", " ", " ", " ", " "] 
+display_board(board_3)
+
+board_4 = ["O", " ", " ", " ", "X", " ", " ", " ", " "]
+display_board(board_4)
+
+board_5 = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+display_board(board_5)
+
+board_6 = [" ", " ", " ", " ", " ", " ", "O", "O", "O"]
+display_board(board_6)
+
+board_7 = ["X", " ", " ", " ", "X", " ", " ", " ", "X"]
+display_board(board_7)
+
+board_8 = [" ", " ", "O", " ", "O", " ", "O", " ", " "]
+display_board(board_8)
+
+board_9 = ["X", "O", "O", " ", " ", " ", "X", " ", " "]
+display_board(board_9)
+
+board_10 = ["X", "X", "X", "X", "X", "X", "X", "X", "X"] 
+display_board(board_10)
+
+board_11 = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]
+display_board(board_11)
